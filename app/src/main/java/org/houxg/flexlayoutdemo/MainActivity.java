12110d090e8d3cb19324c4b8994a500c8ef82628
houxg <houxg09@foxmail.com>
2016-08-02 13:09:03 +0800

    rebuild the project; support jitpack

---
+++ app/src/main/java/org/houxg/flexlayoutdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package org.houxg.flexlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(org.houxg.flexlayoutdemo.R.layout.activity_main);
+    }
+}
